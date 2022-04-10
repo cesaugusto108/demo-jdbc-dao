d37f4f4d9b59db17cda5affff67e2eec845b9819
César Augusto <ces.augusto108@gmail.com>
2022-04-09 21:18:03 -0300

    Department class created

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,11 @@
 package application;
 
+import model.entities.Department;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Department department = new Department(7, "Auto");
+        System.out.println(department);
     }
 }
