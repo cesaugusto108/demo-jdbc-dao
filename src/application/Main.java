31f8f4c73f3bbbbac5dc420f79ba0b2b24a5f59d
César Augusto <ces.augusto108@gmail.com>
2022-04-09 21:48:38 -0300

    Salesperson class created.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,27 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Salesperson;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
         Department department = new Department(7, "Auto");
         System.out.println(department);
+
+        Salesperson salesperson = new Salesperson(
+                1,
+                "Joseph Smith",
+                "joseph@email.com",
+                new Date(simpleDateFormat.parse("01/01/1988").getTime()),
+                3000.00,
+                7,
+                department);
+        System.out.println(salesperson);
     }
 }
---
+++ src/model/entities/Salesperson.java
@@ -0,0 +1,107 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Salesperson implements Serializable {
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+    private Integer departmentId;
+
+    private Department department;
+
+    public Salesperson() {
+    }
+
+    public Salesperson(Integer id, String name, String email, Date birthDate, Double baseSalary, Integer departmentId, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.departmentId = departmentId;
+        this.department = department;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Salesperson that)) return false;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Salesperson id: " + id + ";\n"
+                + "Name: " + name + ";\n"
+                + "Email: " + email + ";\n"
+                + "Birth date: " + birthDate + ";\n"
+                + "Base salary: " + baseSalary + ";\n"
+                + "Department id: " + departmentId;
+    }
+}
